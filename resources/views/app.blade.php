9a2bac190f0aa254cd2866a30830fdb0ac64cb46
Ador1717 <adordawit4@gmail.com>
2025-12-16 17:48:53 +0100

    chanage the header to stegif V2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description"
-          content="StegIF is an educational game that introduces students to steganography in a fun and interactive way.">
+        content="StegIF, the game designed to give students an introduction
+        to the topic of steganography, while having fun. Perfect for students learning
+        about cybersecurity without preknowledge on steganography.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'StegIF') }}</title>
-
+    <title>StegIF</title>
+    
     @vite('resources/css/app.css')
 </head>
 
